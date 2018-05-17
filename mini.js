ecef965360c6d4301bfd771ba4bc305dec81dddb
Priit Parmakson <priitp@ria.ee>
2018-05-18 02:28:19 +0300

    +

--- mini.js
+++ mini.js
@@ -8,8 +8,6 @@ var https = require('https');
 var fs = require('fs');
 var path = require('path');
 
-const app = express();
-
 var keyPath = path.join(__dirname, 'keys', 'tara-stat.key');
 var certPath = path.join(__dirname, 'keys', 'tara-stat.cert');
 var options = {
@@ -27,8 +25,7 @@ http.createServer(function (req, res) {
 */
 
 /* HTTPS serveri loomine */
-var port = 5000;
-var server = https.createServer(options, app);
-server.listen(port, function () {
-  console.log('--- TARA-Stat kuulab pordil: ' + server.address().port);
-});
+https.createServer(options, (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('TARA-Stat - Minimaalne test OK');
+}).listen(5000);
