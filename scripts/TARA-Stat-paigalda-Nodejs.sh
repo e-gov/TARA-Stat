63797ca0ff367c1d0054817851e8c15be831e7cd
Priit Parmakson <priitp@ria.ee>
2018-05-17 20:48:03 +0300

    +

--- scripts/TARA-Stat-paigalda-Nodejs.sh
+++ scripts/TARA-Stat-paigalda-Nodejs.sh
@@ -9,17 +9,10 @@ echo
 echo " --- TARA-Stat: Paigaldan Node.js ja TARA-Stat veebirakendus"
 echo
 
-# 1. Kaitse eksliku käivitamise vastu
-# 2. Paigaldan Node.js
-# 3. Paigaldan curl-i
-# 4. Paigaldan Node.js
-# 5. Paigaldan rakendusele vajalikud Node.js teegid
-# 6. Genereerin ja paigaldan veebirakenduse HTTPS võtmed
-# NB! Praegu mitte tehtud -> 7. Loon kasutaja tarastat
-# 8. Paigaldan MongoDB kasutamise salasõna
-# 9. Loon usalduse TARA-Serveri ja TARA-Stat-i vahel
-# Praegu välja lülitatud:
-# 10. Paigaldan Node.js protsessihalduri pm2
+# 0. Kontrollküsimus eksliku käivitamise vastu
+# 1. Kontrollin, kas Node.js on juba paigaldatud
+# 2. Paigaldan curl-i
+# 3. Paigaldan Node.js
 
 # ------------------------------
 # Abistaja: Väljasta lõputeade ja välju
@@ -68,33 +61,25 @@ function paigalda_Nodejs_teek {
 }
 
 # ------------------------------
-# 1. Kaitse eksliku käivitamise vastu
+# 0. Kontrollküsimus eksliku käivitamise vastu
+#
 kasJatkan
 
 # ------------------------------
-# Kontrollin, kas Node.js töötab. Kui töötab, siis seiskan
-# NODEJS_PID=$(pidof nodejs)
-# if [ ! -z "$NODEJS_PID" ]
-# then
-#   kill -s 15 $NODEJS_PID
-#   echo " --- TARA-Stat veebirakendus seisatud"
-# fi
-
-# ------------------------------
-# 2. Kontrollin, kas Node.js on paigaldatud
+# 1. Kontrollin, kas Node.js on paigaldatud
 #
 echo
 echo Kontrollin, kas Node.js on paigaldatud
 # dpkg -s tagastab 0, kui pakett on paigaldatud; 1, kui ei ole
 dpkg -s nodejs &> /dev/null
 if [ "$?" = 0 ]; then 
-  echo " --- Eemaldan Node.js"
-  sudo apt-get remove nodejs
-  kontrolli "$?" "Node.js eemaldamine"
+  echo " --- Node.js on juba paigaldatud"
+  echo "     Eemaldamiseks sisesta: sudo apt-get remove nodejs"
+  lopeta  
 fi
 
 # ------------------------------
-# 3. Paigaldan curl-i
+# 2. Paigaldan curl-i
 #
 echo
 echo " --- Paigaldan curl-i"
@@ -102,7 +87,7 @@ sudo apt-get install curl
 kontrolli "$?" "curl-i paigaldamine"
 
 # ------------------------------
-# 4. Paigaldan Node.js
+# 3. Paigaldan Node.js
 #
 echo
 echo " --- Paigaldan Node.js"
@@ -113,32 +98,11 @@ sudo apt-get install -y nodejs
 kontrolli "$?" "Node.js paigaldamine"
 
 echo " --- Kontrolli Node.js paigaldust:"
+echo
 nodejs -v
 npm --version
 
 echo
-kasJatkan
-
-# ------------------------------
-# 10. Paigaldan Node.js protsessihalduri pm2
-#
-# Praegu välja lülitatud
-
-lopeta
-
-echo
-echo "Paigaldan Node.js protsessihalduri pm2"
-sudo npm install -g pm2
-kontrolli "$?" "Protsessihalduri pm2 paigaldamine"
-
-echo
-echo "Genereerin pm2 automaatkäivituse skripti"
-pm2 startup systemd
-
-echo
-echo "Täida eelmise käsu väljundi viimane rida (sellega luuakse systemd unit, millega pm2 automaatkäivitatakse)"
-
-echo "Seejärel pm2 start index käivitab TARA-Stat veebirakenduse"
 
 lopeta
 
