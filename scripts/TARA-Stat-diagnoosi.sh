3babcafb2d555c1beaa4825efe33b80e59fa118b
Priit Parmakson <priitp@ria.ee>
2018-05-17 23:51:26 +0300

    +

--- scripts/TARA-Stat-diagnoosi.sh
+++ scripts/TARA-Stat-diagnoosi.sh
@@ -9,19 +9,23 @@ echo
 echo " --- TARA-Stat: Annan teavet paigalduse olukorra kohta"
 echo
 
-systemctl status tarasta
-ps aux | grep tarasta
+echo " ----------------------------------------------- "
+systemctl status tarastat
+echo " ----------------------------------------------- "
+# ps aux | grep tarasta
 systemctl status mongodb
-ps aux | grep mongodb
+echo " ----------------------------------------------- "
+# ps aux | grep mongodb
 
 echo "            MongoDB                                     "
-echo "| konf-ifail  | `/etc/mongodb.conf`                    |"
-echo "| logi        | `/var/log/mongodb/mongod.log`          |"
-echo "| baasifailid | `var/lib/mongodb`                      |"
-echo "| haldusüksus | `/lib/systemd/system/mongod.service`   |"
+echo "| konf-ifail  | /etc/mongodb.conf                    |"
+echo "| logi        | /var/log/mongodb/mongod.log          |"
+echo "| baasifailid | var/lib/mongodb                      |"
+echo "| haldusüksus | /lib/systemd/system/mongod.service   |"
+echo
 
 echo "            TARA-Stat                                   "
-echo "| konf-ifail  | `/opt/TARA-Stat/config.js`             |"
-echo "| logi        | `/opt/TARA-Stat/log.txt`               |"
-echo "| baasifailid | `var/lib/mongodb`                      |"
-echo "| haldusüksus | `/lib/systemd/system/tarastat.service` |"
+echo "| konf-ifail  | /opt/TARA-Stat/config.js             |"
+echo "| logi        | /opt/TARA-Stat/log.txt               |"
+echo "| baasifailid | var/lib/mongodb                      |"
+echo "| haldusüksus | /lib/systemd/system/tarastat.service |"
