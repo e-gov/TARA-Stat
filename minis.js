092eb7c9bcaa311fa9eaa3f1dd25ecab2e69c860
Priit Parmakson <priitp@ria.ee>
2018-05-18 03:12:58 +0300

    +

---
+++ minis.js
@@ -0,0 +1,31 @@
+/*
+  TARA-Stat - Minimaalne test
+*/
+
+'use strict'; 
+let http = require('http'); 
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+
+var keyPath = path.join(__dirname, 'keys', 'tara-stat.key');
+var certPath = path.join(__dirname, 'keys', 'tara-stat.cert');
+var options = {
+  key: fs.readFileSync(keyPath, 'utf8'),
+  cert: fs.readFileSync(certPath, 'utf8'),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
+/*
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('TARA-Stat - Minimaalne test OK'); 
+}).listen(5001);
+*/
+
+/* HTTPS serveri loomine */ 
+https.createServer(options, (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('TARA-Stat - Minimaalne test OK');
+}).listen(5000);
