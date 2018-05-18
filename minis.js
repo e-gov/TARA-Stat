0ed26547424d8d78a431d4aa4d9c79cd6ceacf43
Priit Parmakson <priitp@ria.ee>
2018-05-18 03:19:48 +0300

    +

--- minis.js
+++ minis.js
@@ -21,7 +21,7 @@ var options = {
 http.createServer(function (req, res) { 
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('TARA-Stat - Minimaalne test OK'); 
-}).listen(5001);
+}).listen(443);
 */
 
 /* HTTPS serveri loomine */ 
