a1cb11ce26e22765494427308c995e1cfa6f94e4
Priit Parmakson <priitp@ria.ee>
2018-08-02 16:43:44 +0300

    +

--- TCP-Klient-test.js
+++ TCP-Klient-test.js
@@ -8,8 +8,17 @@
 'use strict';
 const net = require('net');
 
-let client = net.connect({ port: 3000 });
+let client = net.Socket();
+client.connect(3000, '127.0.0.1', function() {
+  console.log('TARA-Stat-ga ühendus loodud');
+  client.write('See on logikirje.');
+});
 
-client.on('data', (data) => {
-  console.log(data.toString());
+client.on('data', function(data) {
+	console.log('Saadud: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('TARA-Stat-ga ühendus suletud');
 });
